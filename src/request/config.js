a8299ce290479ff2d1eedd1ce75952a9f1049f09
pingshiguo <pingshiguo@gmail.com>
2019-11-20 17:19:06 +0800

    增加三级联动数据请求地址

--- src/request/config.js
+++ src/request/config.js
@@ -1,9 +1,14 @@
-export const BASE_URL = '/api'
+const isProd = process.env.NODE_ENV === 'production'
+
+export const BASE_URL = isProd ? '/api' : '/api'
+
+export const CHINA_URL = isProd ? '/static/lib/china.json' : '/static/lib/china.json'
 
 export const SUCCESS_CODE = '1'
 
 export const FAILURE_CODE = '-1'
 
 export default {
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+  chinaURL: CHINA_URL
 }
